c67c6a175308f3778c71175329e330c83ab44656
Gajendra Naphade <118757883+gajju44@users.noreply.github.com>
2024-07-21 18:26:06 +0530

    Update result.php for local machine

--- result.php
+++ result.php
@@ -29,7 +29,7 @@ session_start();
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Feedback show</title>
+    <title>result</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
