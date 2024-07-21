cd6c86242f0cc7f05240573478db409a28fb05a9
Gajendra Naphade <118757883+gajju44@users.noreply.github.com>
2024-07-21 17:46:08 +0530

    Update add_question_faculty.php for local machine

--- add_question_faculty.php
+++ add_question_faculty.php
@@ -17,7 +17,6 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.-->
-
 <?php
 error_reporting(0);
 require_once('Config.php');
